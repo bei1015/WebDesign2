fc6518d27dc91fba2b22420a93f2ea01f51a9283
g51014 <a912686931@gmail.com>
2017-04-26 13:09:44 +0800

    新增年度計畫

---
+++ plan/js/feature.js
@@ -0,0 +1,92 @@
+$(".home").click(function(){
+  $(".home span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".home_text").slideToggle(500);
+});
+// $(".home").click(function(){
+//   $(".home_text").slideUp(500);
+// });
+
+$(".TA").click(function(){
+  $(".TA span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".TA_text").slideToggle(500);
+});
+
+$(".CSIE").click(function(){
+  $(".CSIE span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".CSIE_text").slideToggle(500);
+});
+
+$(".train").click(function(){
+  $(".train span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".train_text").slideToggle(500);
+});
+
+$(".web").click(function(){
+  $(".web span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".web_text").slideToggle(500);
+});
+
+$(".go").click(function(){
+  $(".go span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".go_text").slideToggle(500);
+});
+
+$(".day").click(function(){
+  $(".day span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".day_text").slideToggle(500);
+});
+
+$(".summer").click(function(){
+  $(".summer span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".summer_text").slideToggle(500);
+});
+
+$(".new").click(function(){
+  $(".new span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".new_text").slideToggle(500);
+});
+
+$(".party").click(function(){
+  $(".party span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".party_text").slideToggle(500);
+});
+
+$(".escape").click(function(){
+  $(".escape span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".escape_text").slideToggle(500);
+});
+
+$(".eat").click(function(){
+  $(".eat span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".eat_text").slideToggle(500);
+});
+
+$(".game").click(function(){
+  $(".game span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".game_text").slideToggle(500);
+});
+
+$(".winter").click(function(){
+  $(".winter span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".winter_text").slideToggle(500);
+});
+
+$(".tail").click(function(){
+  $(".tail span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".tail_text").slideToggle(500);
+});
